bcfb9782e388eb1374f7dc794c962a3e1611d8e6
Daniel <brikbusters@gmail.com>
2021-05-08 23:33:44 -0400

    "Bad Apple" Demo

--- ahdk/gopro.h
+++ ahdk/gopro.h
@@ -1,14 +1,41 @@
 #ifndef __GOPRO
 #define __GOPRO
 
-// Actual size is 56x80
-// Screen is a bit field
+// Width is 7 pixels * 8 bits, so 56 pixels
+// Height is 90 layers
 #define SCREEN_WIDTH 7
-#define SCREEN_HEIGHT 10
+#define SCREEN_HEIGHT 80
 
 // fpstring("HELLO", 0, 1);
 void fpstring(char *text, unsigned int a, int b);
-
 void showBuffer(unsigned char *buffer);
+
+
+unsigned char screenBuf[1000];
+
+void setPixel(int x, int y) {
+	screenBuf[y * SCREEN_WIDTH + (x >> 3)] |= 1 << (x % 8);
+}
+
+#define FRAMES 1079
+#define DELAY 30
+
+void playVideo() {
+	int i = 0;
+	FILE *f = fopen("d:/video.bin", "r");
+	while (1) {
+		if (i == FRAMES) {
+			fclose(f);
+			f = fopen("d:/video.bin", "r");
+			i = 0;
+		} else {
+			fread(&screenBuf, (SCREEN_WIDTH * 8) * SCREEN_HEIGHT / 7, 1, f);
+			showBuffer(screenBuf);
+			i++;
+		}
+
+		msleep(DELAY);
+	}
+}
 
 #endif
--- ahdk/test.c
+++ ahdk/test.c
@@ -5,39 +5,6 @@
 #include "ahdk.h"
 #include "gopro.h"
 
-unsigned char screenBuf[1000];
-
-#if 0
-// NOTE: Not working
-void setPixel(int x, int y) {
-	screenBuf[y * SCREEN_WIDTH + (x >> 3)] |= 1 << (x % 8);
-}
-#endif
-
 void start() {
-	for (int i = 0; i < 1000; i++) {
-		screenBuf[i] = 0;
-	}
-
-	screenBuf[0] = 255;
-
-	char buffer[20];
-	int i = 0;
-	
-	while (1) {
-		sprintf(buffer, "d:/F/F%d", i);
-
-		FILE *f = fopen(buffer, "r");
-		fread(&screenBuf, 1000, 1000, f);
-		fclose(f);
-
-		showBuffer(screenBuf);
-		
-		msleep(250);
-		if (i == 25) {
-			i = 0;
-		} else {
-			i++;
-		}
-	}
+	playVideo();
 }
