9b49b738c619b6c8d32dfe667571b9f971c8c4b3
Daniel <daniel@thinkpadr52>
2021-07-01 14:31:48 -0400

    Hot reloading script

--- ahdk/Test.sh
+++ ahdk/Test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-make minimalonly file=standalone.c NOWRITE=1
+make minimalonly file=standalone.c NOWRITE=1 NOCLEAN=1
 echo "!run" > temp
 printf 'root\nput test.bin test.bin\nput temp temp\nexit\n' | ftp 192.168.42.1
 rm temp
